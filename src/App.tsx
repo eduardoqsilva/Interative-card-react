1344f47e812c3a34ad70c37483db57e9a16ecdf3
eduardoqsilva <eduadsilva@outlook.com>
2023-02-06 20:44:23 -0300

    First commit :tada:

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { CardFront } from "./components/Card/Cards"
+import { GlobalStyle } from "./styles/global"
+
+function App() {
+
+  return (
+    <>
+      <GlobalStyle/>
+      <CardFront/>
+    </>
+  )
+}
+
+export default App
---
+++ src/styles/global.ts
@@ -0,0 +1,9 @@
+import { createGlobalStyle, GlobalStyleComponent } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`
